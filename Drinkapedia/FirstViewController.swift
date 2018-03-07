02518a47e1d7d7192012b84a5251b9d5d77688e5
Advith Chegu <advil64@gmail.com>
2018-03-06 23:22:28 -0500

    Initial Commit

---
+++ Drinkapedia/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Drinkapedia
+//
+//  Created by Advith Chegu on 3/6/18.
+//  Copyright © 2018 Advith Chegu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
